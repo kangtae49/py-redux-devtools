14240155274d888c146d5c411e12155dc4e0f191
kangtae49 <kangtae49@gmail.com>
2025-11-10 05:36:29 +0900

    add refresh(F5 or Ctrl+r)

--- src-react/src/App.tsx
+++ src-react/src/App.tsx
@@ -1,6 +1,23 @@
+import {useEffect} from "react";
 import { Root } from '@redux-devtools/app';
 import "./App.css"
 function App() {
+  useEffect(() => {
+
+    const handleKeyDown = (e: KeyboardEvent) => {
+      const isRefreshKey =
+        (e.key === 'F5') ||
+        (e.key === 'r' && (e.ctrlKey || e.metaKey));
+
+      if (isRefreshKey) {
+        e.preventDefault();
+        window.location.reload()
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, []);
   return (
     <Root />
   )
